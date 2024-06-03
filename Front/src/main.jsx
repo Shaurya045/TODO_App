5497ac6778e4db5bbf3aa3f83126abeecf232067
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2024-06-03 19:24:14 +0530

    Connecting backend to frontend

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "https://nodejs-todoapp.onrender.com/api/v1";
+export const server = "https://todo-app-9b0o.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
